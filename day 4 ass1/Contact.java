04e5594eb8b4e6c75e04b7a6942e0cf689d96b13
divyalakshmi1011 <divya.vallabhapurapu@gmail.com>
2019-09-27 10:20:21 +0530

    Contact object is done

--- day 4 ass1/Contact.java
+++ day 4 ass1/Contact.java
@@ -9,6 +9,52 @@
  */
 
 //  Your code goes here... For Contact class
-public class contact {
-    public 
-}
+public class Contact 
+{ 
+    // Instance Variables 
+    String name; 
+    String email; 
+    Long phoneNumber;  
+  
+    // Constructor Declaration of Class 
+    public Contact(String name, String email, 
+                   Long phoneNumber) 
+    { 
+        this.name = name; 
+        this.email = email; 
+        this.phoneNumber = phoneNumber;  
+    } 
+  
+    // method 1 
+    public String getName() 
+    { 
+        return name; 
+    } 
+  
+    // method 2 
+    public String getemail() 
+    { 
+        return email; 
+    } 
+  
+    // method 3 
+    public Long getphoneNumber() 
+    { 
+        return phoneNumber; 
+    } 
+  
+    public String toString() 
+    { 
+        return("Name"+ this.getName()+ 
+               ".\nemail" + this.getemail()+".\nPhone Number"+ this.getphoneNumber()); 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        Contact c1 = new Contact("Divya","divya@3", 34566781098L);
+        Contact c2 = new Contact("Himaja","himaja@1", 1234567L);
+        System.out.println(c1.toString());
+        System.out.println("=============================");
+        System.out.println(c2.toString());
+    } 
+} 
